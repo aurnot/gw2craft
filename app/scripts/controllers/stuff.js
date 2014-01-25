5bfab005be7d8ad56e1a433fd52b4aec50290f60
aurnot <aurnot@gmail.com>
2014-01-25 11:18:13 +0100

    feat(core): add stats calculator, supports base stats and profession

--- app/scripts/controllers/stuff.js
+++ app/scripts/controllers/stuff.js
@@ -1,15 +1,23 @@
 'use strict';
 
 angular.module('gw2craftApp')
-  .controller('StuffCtrl', function ($scope, $http, _) {
-    console.log(_.contains([4, 8, 15, 16, 23, 42], 42));
+  .controller('StuffCtrl', function ($scope, $http, _, calculator) {
 
     // load professions
     $http.get('data/professions.json').success(function(data) {
-        $scope.professions = data;
+      $scope.professions = data;
     });
 
     // holds all character stats
     $scope.stats = {};
+
+    /**
+     * Sets the current profession to the given one, and refreshes stats
+     * @param  {Object} profession
+     */
+    $scope.setProfession = function(profession) {
+      calculator.profession = profession;
+      $scope.stats = calculator.update();
+    };
 
   });
---
+++ app/scripts/services/calculator.js
@@ -0,0 +1,97 @@
+'use strict';
+
+angular.module('gw2craftApp')
+  .factory('calculator', function(_) {
+    return {
+      stats : {},
+
+      baseStats : {
+        // primary
+        power : 916,
+        precision : 916,
+        toughness : 916,
+        vitality : 916,
+
+        // secondary
+        condDamage : 0,
+        critDamage : 0,
+        healPower : 0,
+
+        // derived
+        attack : 0,
+        critChance : 0,
+        armor : 0,
+        health : 0,
+
+        // other
+        defense : 0
+      },
+
+      profession : null,
+
+      /**
+       * Updates the stats for current profession, then returns them
+       *
+       * @return {Object}
+       */
+      update : function() {
+        this._applyBaseStats();
+        this._applyProfessionStats();
+        this._calculateStats();
+
+        return this.stats;
+      },
+
+      /**
+       * Resets stats to their base value
+       *
+       * @protected
+       */
+      _applyBaseStats : function() {
+        var self = this;
+        _.each(self.baseStats, function(value, key) {
+          self.stats[key] = value;
+        });
+      },
+
+      /**
+       * Applies any profession-related stat modification
+       *
+       * @protected
+       */
+      _applyProfessionStats : function() {
+        if (this.profession) {
+          this.stats.defense += this.profession.defense;
+          this.stats.health += this.profession.health;
+        }
+      },
+
+      /**
+       * Calculates derived stats
+       *
+       * @protected
+       */
+      _calculateStats : function() {
+        // attack
+        var power = this.stats.power;
+        var attack = power;
+        this.stats.attack = attack;
+
+        // crit chance
+        var precision = this.stats.precision;
+        var critChance = Math.floor((precision - 822) / 21);
+        this.stats.critChance = critChance;
+
+        // armor
+        var toughness = this.stats.toughness;
+        var defense = this.stats.defense;
+        var armor = toughness + defense;
+        this.stats.armor = armor;
+
+        // health
+        var vitality = this.stats.vitality;
+        var health = 10 * vitality;
+        this.stats.health += health; // add vita health to base pool
+      }
+    };
+  });
